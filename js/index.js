f86cabc5fd40ed4ff0c5f6d4f360aee5bffcf049
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-10-10 16:56:40 +0200

    10oct14 added: crearRecetas/ added:new js functionality

--- js/index.js
+++ js/index.js
@@ -2,14 +2,22 @@
 title: string     --->Recipe Title
 img: string       ---->Image file
 description: string -->Recipe description
+delivery: int -->Delivery time (minuts)
+prep: int -->Preparation time (minuts)
+ingredients: array -->Ingredients list
+like: int -->Number of likes/dislikes
 */
-function newRecipe(title, img, description){
+function newRecipe(title, img, description, delivery, prep, ingredients){
 	var Recipe = {
 		init: function(title, img, description){
 			'use strict';
 			this.title = title;
 			this.img = img;
 			this.description = description;
+			this.delivery = delivery;
+			this.prep = prep;
+			this.ingredients = ingredients;
+			this.like = 0;
 
 		}
 
@@ -24,33 +32,60 @@ Estos arrays se podrian rellenar a partir de un json una vez tengamos backend??
 */
 var mealRecipes = [newRecipe('Tacos de cerdo con tocineta',
 	'./images/carnivoros/tacosCerdo.jpg',
-	'Corta el lomo de cerdo en tacos y sazona. Extiende las lonchas de tocineta y enrosca con cada una un taco de cerdo. Pincha con un palillo para que no se suelten y cocina en una sartén con un chorrito de aceite. Para preparar la salsa de vino tinto: pela y pica una cebolleta y pon a pochar en una sartén con un chorro de aceite y una pizca de sal. Después, añade la nata y el vino y deja reducir. Sirve los tacos de cerdo con la tocineta frita y salsea con la salsa de vino. Espolvorea con perejil picado y ¡a disfrutar! Consejo sobre la receta de Tacos de cerdo con tocineta: Este plato es muy graso por lo que se recomienda un consumo moderado. Esta receta se puede acompañar con trozos de piña, una fruta que le va muy bien al lomo de cerdo. La tocineta o panceta le dará al lomo de cerdo un toque crujiente muy sabroso.'),
+	'Corta el lomo de cerdo en tacos y sazona. Extiende las lonchas de tocineta y enrosca con cada una un taco de cerdo. Pincha con un palillo para que no se suelten y cocina en una sartén con un chorrito de aceite. Para preparar la salsa de vino tinto: pela y pica una cebolleta y pon a pochar en una sartén con un chorro de aceite y una pizca de sal. Después, añade la nata y el vino y deja reducir. Sirve los tacos de cerdo con la tocineta frita y salsea con la salsa de vino. Espolvorea con perejil picado y ¡a disfrutar! Consejo sobre la receta de Tacos de cerdo con tocineta: Este plato es muy graso por lo que se recomienda un consumo moderado. Esta receta se puede acompañar con trozos de piña, una fruta que le va muy bien al lomo de cerdo. La tocineta o panceta le dará al lomo de cerdo un toque crujiente muy sabroso.',
+	15,
+	15,
+	["Cerdo", "Tocino", "Aceite","Sal"]),
 	newRecipe('Costillas de cordero adobadas',
 		'./images/carnivoros/costillaCerdo.jpg',
-		'El cordero, como el cerdo, va muy bien con la piña, como en esta receta de costillar de cordero asado con piña. Sin embargo, en esta ocasión se adoba la carne con una mezcla de pimentón, jengibre, ajo y comino macerado durante 2 horas para que coja todo el sabor del adobo. También es muy habitual darle al cordero distintos sabores con especias, en este costillar, además del pimentón, el jengibre y el comino para el adobo, se utiliza cilantro para salsear el plato.'),
+		'El cordero, como el cerdo, va muy bien con la piña, como en esta receta de costillar de cordero asado con piña. Sin embargo, en esta ocasión se adoba la carne con una mezcla de pimentón, jengibre, ajo y comino macerado durante 2 horas para que coja todo el sabor del adobo. También es muy habitual darle al cordero distintos sabores con especias, en este costillar, además del pimentón, el jengibre y el comino para el adobo, se utiliza cilantro para salsear el plato.',
+		20,
+		20,
+		["Costillas", "Piña", "Aceite", "Sal"]),
 	newRecipe('Rollitos de ternera',
 		'./images/carnivoros/rollitosTernera.jpg',
-		'Extiende los filetes de ternera sobre un plato, sazónalos y coloca encima de cada uno una loncha de jamón serrano, otra de queso, cubre con otra de jamón y un poco de salsa de tomate. Enrosca y pincha con un palillo para que no se suelten. Fríe los rollitos de ternera en una sartén con un poco de aceite con un par dientes de ajo aplastados. Limpia las hojas de rúcula, alíñalas con aceite, sal y vinagre. Sirve un rollito de ternera por comensal, acompañado de la ensalada de rúcula. Consejo de la receta de Rollitos de ternera: Esta receta de rollitos de ternera se puede sustituir la ternera por pollo. A la hora de enrollar la ternera pincha los rollitos con un palillo para que a la hora de freírlos en la sartén no se desenrosquen.'),
+		'Extiende los filetes de ternera sobre un plato, sazónalos y coloca encima de cada uno una loncha de jamón serrano, otra de queso, cubre con otra de jamón y un poco de salsa de tomate. Enrosca y pincha con un palillo para que no se suelten. Fríe los rollitos de ternera en una sartén con un poco de aceite con un par dientes de ajo aplastados. Limpia las hojas de rúcula, alíñalas con aceite, sal y vinagre. Sirve un rollito de ternera por comensal, acompañado de la ensalada de rúcula. Consejo de la receta de Rollitos de ternera: Esta receta de rollitos de ternera se puede sustituir la ternera por pollo. A la hora de enrollar la ternera pincha los rollitos con un palillo para que a la hora de freírlos en la sartén no se desenrosquen.',
+		10,
+		10,
+		["Ternera", "Jamón Serrano", "Queso", "Aceite", "Sal"]),
 	];
 var vegetarianRecipes = [newRecipe('Arroz con verduras',
 		'./images/vegetarianos/arrozVerduras.jpg',
-		'Se pone a hervir en una cazuela 4 puñados de arroz integral basmati para 2 personas, unos 40 minutos, en otra sartén con poco aceite de oliva se cocina a fuego lento espárragos, pimiento rojo y verde junto dos hojas de laurel, una vez pocha la verdura se añade la cebolla y se cocina a fuego lento todo. Cuando el arroz este en su punto de cocción, se escurre y se añade a la sartén junto a las verduras, se deja unos minutos en la sartén para poder mezclar bien los ingredientes con los sabores en la sartén,se rectifica de sal y pimienta si hiciera falta.'),
+		'Se pone a hervir en una cazuela 4 puñados de arroz integral basmati para 2 personas, unos 40 minutos, en otra sartén con poco aceite de oliva se cocina a fuego lento espárragos, pimiento rojo y verde junto dos hojas de laurel, una vez pocha la verdura se añade la cebolla y se cocina a fuego lento todo. Cuando el arroz este en su punto de cocción, se escurre y se añade a la sartén junto a las verduras, se deja unos minutos en la sartén para poder mezclar bien los ingredientes con los sabores en la sartén,se rectifica de sal y pimienta si hiciera falta.', 
+		5,
+		5,
+		["Arroz", "Aceite de Oliva", "Sal"]),
 	newRecipe('Champis rellenos',
 		'./images/vegetarianos/champisRellenos.jpg',
-		'Limpiar los champiñones y cortarles la cola. Trocearla y guardarla.  Picar las cebollas y el pimiento. Junto con las colas de los champiñones ponerlo en una sartén a fuego medio con un poco de aceite. Darle unas vueltas hasta que empiece a cambiar de color. En una bandeja para el horno, poner un poco de aceite en el fondo. Los champiñones y encima las verduras rehogadas. Salpimentamos. Para terminar, lo espolvoreamos con el pan rallado y lo metemos en el horno unos 10 minutos a 180 o hasta que se doren.'),
+		'Limpiar los champiñones y cortarles la cola. Trocearla y guardarla.  Picar las cebollas y el pimiento. Junto con las colas de los champiñones ponerlo en una sartén a fuego medio con un poco de aceite. Darle unas vueltas hasta que empiece a cambiar de color. En una bandeja para el horno, poner un poco de aceite en el fondo. Los champiñones y encima las verduras rehogadas. Salpimentamos. Para terminar, lo espolvoreamos con el pan rallado y lo metemos en el horno unos 10 minutos a 180 o hasta que se doren.', 
+		25,
+		25,
+		["Champiñones", "cebollas", "pimiento", "Aceite", "Sal"]),
 	newRecipe('Cocarrois de verdura',
 		'./images/vegetarianos/cocarrois.jpg',
-		'En un bol añadir la harina y hacer un hueco en el centro para verter los líquidos (cerveza y aceite), con las manos mezclar bien ambos componentes hasta formar una masa manejable, amasando para que quede firme. Picar a trozos pequeños los ingredientes del relleno y aliñarlos con el aceite y el pimentón antes de meter en la masa.')
+		'En un bol añadir la harina y hacer un hueco en el centro para verter los líquidos (cerveza y aceite), con las manos mezclar bien ambos componentes hasta formar una masa manejable, amasando para que quede firme. Picar a trozos pequeños los ingredientes del relleno y aliñarlos con el aceite y el pimentón antes de meter en la masa.', 
+		13,
+		13,
+		["Harina", "Cerveza","Aceite", "Sal"])
 	];
 var dessertRecipes = [newRecipe('Bizcocho ligero de Chocolate con Mango y Lima ',
 		'./images/postres/bizcochoChocolate.jpg',
-		'Rallar finamente la cáscara de la piel de la lima. Pelar y cortar los mangos en dados. En un bol, partir el chocolate, añadir la mantequilla en trozos y fundirlo en el microondas 2 minutos a 500W. Agregar los huevos, el azúcar y la harina. Mezclar bien y añadir la levadura y la ralladura de la lima. Verter la mezcla en un molde cuadrado, apto para el microondas, con mantequilla y enharinado. Añadir el mango picado por encima, presionando ligeramente. Cocerlo en el microondas de 9 a 10 minutos a 900 W.'),
+		'Rallar finamente la cáscara de la piel de la lima. Pelar y cortar los mangos en dados. En un bol, partir el chocolate, añadir la mantequilla en trozos y fundirlo en el microondas 2 minutos a 500W. Agregar los huevos, el azúcar y la harina. Mezclar bien y añadir la levadura y la ralladura de la lima. Verter la mezcla en un molde cuadrado, apto para el microondas, con mantequilla y enharinado. Añadir el mango picado por encima, presionando ligeramente. Cocerlo en el microondas de 9 a 10 minutos a 900 W.', 
+		7,
+		7,
+		["Lima", "Mango", "Chocolate", "mantequilla"]),
 	newRecipe('Crema de chocolate',
 		'./images/postres/cremaChocolate.jpg',
-		'En un bol,  partir el chocolate en trozos pequeños. Calentar la leche evaporada sin que llegue a hervir. Verterla sobre el chocolate, cubriéndolo todo y tapar el recipiente con una tapa o plato. Esperar 2 minutos y mezclar fuertemente con un tenedor o un batidor. Dejarla reposar en el refrigerador para que se solidifique un poco.'),
+		'En un bol,  partir el chocolate en trozos pequeños. Calentar la leche evaporada sin que llegue a hervir. Verterla sobre el chocolate, cubriéndolo todo y tapar el recipiente con una tapa o plato. Esperar 2 minutos y mezclar fuertemente con un tenedor o un batidor. Dejarla reposar en el refrigerador para que se solidifique un poco.', 
+		8,
+		8,
+		["Chocolate", "leche", "huevos"]),
 	newRecipe('Magdalenas de chocolate',
 		'./images/postres/magdalenasChocolate.jpg',
-		'Precalentar el horno a  200 ° C. Derretir la mantequilla y dejar que se enfríe. En un bol, mezclar los huevos, el azúcar y la harina. Añadir la levadura y la mantequilla. Mezclar y dejar reposar 1 hora 30 en el refrigerador. Engrasar las cápsulas para magdalenas y enharinarlas ligeramente. Llenarlas con la masa hasta la mitad y cocerlas  unos 15 minutos. Derretir el chocolate con leche en el microondas. Desmoldar las magdalenas y bañar un lado con chocolate y espolvorearlo con coco. Colocar en la nevera 30 minutos.')
+		'Precalentar el horno a  200 ° C. Derretir la mantequilla y dejar que se enfríe. En un bol, mezclar los huevos, el azúcar y la harina. Añadir la levadura y la mantequilla. Mezclar y dejar reposar 1 hora 30 en el refrigerador. Engrasar las cápsulas para magdalenas y enharinarlas ligeramente. Llenarlas con la masa hasta la mitad y cocerlas  unos 15 minutos. Derretir el chocolate con leche en el microondas. Desmoldar las magdalenas y bañar un lado con chocolate y espolvorearlo con coco. Colocar en la nevera 30 minutos.', 
+		11,
+		11,
+		["coco", "mantequilla", "azúcar", "Harina"])
 	];
 
 /**
@@ -78,8 +113,46 @@ function changeRecipe(recipeId, recipeObject){
 	container.children[0].src = recipeObject.img;
 	//Description
 	container.children[1].children[1].textContent = recipeObject.description;
-	
+
+	container.children[0].addEventListener("click", changeModal.bind(this, recipeObject));
+
+
 	//Falta añaidir los botones de Pedir e Info
+}
+
+function changeModal(recipesObject){
+	var ingredients = document.getElementById("ingredients");
+	ing = recipesObject.ingredients;
+	ingredients.innerHTML = ""
+	for(i=0; i<ing.length; i++) {
+		var li = document.createElement("li")
+		li.innerHTML=ing[i];
+		ingredients.appendChild(li);
+	}
+
+	var minutes = document.getElementById("recipeTime");
+	minutes.children[0].children[1].innerHTML = recipesObject.prep + " minutos";
+
+	minutes.children[1].children[1].innerHTML = recipesObject.delivery + " minutos";
+    
+    minutes.children[2].children[1].innerHTML = recipesObject.prep + recipesObject.delivery + " minutos";
+
+    var description = document.getElementById("description");
+    description.children[1].innerHTML = recipesObject.description;
+
+    var imgRecipe = document.getElementById("imgRecipe");
+    imgRecipe.src = recipesObject.img;
+
+    var myRecipeTitle1=document.getElementById("myRecipeTitle1");
+    myRecipeTitle1.innerHTML=recipesObject.title;
+
+};
+
+function likeButton (recipesObject){
+	if (this.id === document.getElementById("likeButton"){
+		recipesObject.like = recipesObject + 1;
+
+	}
 }
 
 /* threeRecipes function
