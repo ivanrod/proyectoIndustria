b09bc7979d5ed19a34e5bfbe202910ce9b6c09b3
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-11-06 17:50:08 +0100

    06nov14 Refactoring

--- customCook.rb
+++ customCook.rb
@@ -4,13 +4,20 @@ require 'sinatra/reloader'
 get '/' do 
 	@title = "Custom Cook"
 	@cssFiles = ['"./css/index.css"']
-	@jsFiles = ['"./js/syncJsonDownload.js"', '"./js/index/changeRecipe.js"', '"./js/index/changeModal.js"', '"./js/index/likeButton.js"', '"./js/index/index.js"']
+	@jsFiles = ['"./js/syncJsonDownload.js"', 
+		'"./js/index/changeRecipe.js"', 
+		'"./js/index/changeModal.js"', 
+		'"./js/index/likeButton.js"', 
+		'"./js/index/index.js"']
 	erb :index
 end
 
 get '/crearecetas' do 
 	@title = "Crea tu receta"
 	@cssFiles = ['"./css/crearRecetas.css"']
-	@jsFiles = ['"./js/crearRecetas/syncJsonDownload.js"','"./js/crearRecetas/changeIngredients.js"', '"./js/crearRecetas/dragDrop.js"', '"./js/crearRecetas/crearRecetas.js"']
+	@jsFiles = ['"./js/crearRecetas/syncJsonDownload.js"',
+		'"./js/crearRecetas/changeIngredients.js"', 
+		'"./js/crearRecetas/dragDrop.js"', 
+		'"./js/crearRecetas/crearRecetas.js"']
 	erb :crearRecetas
 end
